50e51487254507358a7465ca2bd11d6ae90f4976
saraa <s.ardovino2@studenti.unisa.it>
2026-05-29 11:31:41 +0200

    task1 task2

--- task.py
+++ task.py
@@ -11,7 +11,10 @@ def prodotto_scalare(v1: list, v2: list) -> float:
 
 def rango_matrice(m: list) -> int:
     """Sub-task 2: Calcola il rango di una matrice."""
-    pass
+    matr=np.array(m)
+    r=np.linalg.matrix_rank(matr)
+    return r
+
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     """Sub-task 3: Risolvere un Sistema Lineare."""
