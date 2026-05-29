1f889a35ddb521313135e3b16e5657f281ebdaad
saraa <s.ardovino2@studenti.unisa.it>
2026-05-29 12:08:43 +0200

    task1 task2 task 3 task 4 non fatta task 5

--- task.py
+++ task.py
@@ -27,9 +27,18 @@ def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
     """Sub-task 4: Correlazione tra Matrici 2x2."""
     pass
 
+
+
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
     """Sub-task 5: Restituisce (seno, coseno, arcoseno, arcocoseno) elemento per elemento calcolati sul primo array."""
-    pass
+    v=np.array(v1)
+    v1=np.sin(v)
+    v2=np.cos(v)
+    v3=np.arcsin(v)
+    v4=np.arccos(v)
+    result=list([v1, v2, v3, v4])
+    return result
+
 
 
 def main():
